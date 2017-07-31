996ffbb14fecac0a0abdacd7c3bda0489ccd4fd4
Sebastian Vizcaino <elvizcacho@gmail.com>
2017-07-31 17:43:29 +0200

    Hacky way Ternary operator

--- TernaryFun.java
+++ TernaryFun.java
@@ -5,19 +5,24 @@ public class TernaryFun {
 
     public static void main(String[] args) {
         boolean randomBoolean = getRandomBoolean();
-        randomBoolean ? eatAKorma() : eatASaag();
+        
+        e((randomBoolean) ? eatAKorma() : eatASaag());
     }
+    
+    private static void e(String a) {}
 
     private static boolean getRandomBoolean() {
         return Math.random() < 0.5;
     }
 
-    private void eatAKorma() {
+    private static String eatAKorma() {
         System.out.println("Eat a chicken korma");
+        return "Eat a chicken korma";
     }
 
-    private void eatASaag() {
+    private static String eatASaag() {
         System.out.println("Eat an aloo saag");
+        return "Eat an aloo saag";
     }
 
 }
